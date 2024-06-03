82b76f717b89c2743b448d71bd8cbbec41067408
Leo Hsieh <LeoHsieh@ami.com>
2024-06-03 13:50:20 +0800

    Update!!

--- backup.bat
+++
@@ -1,6 +0,0 @@
-rmdir /s /q public
-git init
-git branch -M main
-git add --all
-git commit -m "Update!!"
-git push -u origin main
--- depoly.bat
+++ depoly.bat
@@ -1,19 +1,5 @@
-set HUGO_ENV=production
-REM hugo\hugo --gc --minify
-hugo\hugo --gc
-cd public
-git init
-git branch -M master
-git add .
-git commit -m "Update!!"
-git remote add origin git@github.com:theChiChen/Blog.git
-git push -u -f origin master
-cd ..
 rmdir /s /q public
-rmdir /s /q .git
-git init
-git branch -M master
-git add .
+git branch -M main
+git add --all
 git commit -m "Update!!"
-git remote add origin git@github.com:theChiChen/Blog-source.git
-git push -u -f origin master
+git push -u origin main
